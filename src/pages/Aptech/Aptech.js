d42879f94258275a9d8011c409d0fbb289c98e31
Son-KnownPain <sonhong443@gmail.com>
2023-09-27 17:09:06 +0700

    Config layouts for app

---
+++ src/pages/Aptech/Aptech.js
@@ -0,0 +1,7 @@
+function Aptech() {
+    return (
+        <h1>Aptech</h1>
+    );
+}
+
+export default Aptech;
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import Aptech from "~/pages/Aptech"
+import Home from "~/pages/Home"
+
+export const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/aptech',
+        component: Aptech,
+    },
+]
+
+export const privateRoutes = [
+
+]
