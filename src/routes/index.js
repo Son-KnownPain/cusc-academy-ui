c5249ebc451daac88f4fa4bbaca0685d23fa351b
lengochai0207031 <lhai2274@gmail.com>
2023-10-24 11:34:55 +0700

    Contact page init

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
-import config from "~/config"
-import Aptech from "~/pages/Aptech"
-import Home from "~/pages/Home"
+import config from '~/config';
+import Aptech from '~/pages/Aptech';
+import Home from '~/pages/Home';
+import Contact from '~/pages/Contact';
 
 export const publicRoutes = [
     {
@@ -11,8 +12,10 @@ export const publicRoutes = [
         path: config.routes.aptech,
         component: Aptech,
     },
-]
+    {
+        path: config.routes.contact,
+        component: Contact,
+    },
+];
 
-export const privateRoutes = [
-
-]
+export const privateRoutes = [];
